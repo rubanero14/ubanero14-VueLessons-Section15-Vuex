75fc51deebc14b49c8d0311c96f2766ed75a16e0
Rajaruban Rajindram <ahyat888@gmail.com>
2022-03-22 13:04:56 +0800

    deploy

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="addByOne">+1</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    addByOne() {
+      return this.$store.state.counter++;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
